0af197d3309831d661c969e8df1816463f4061de
cakenpeace <chwon02@naver.com>
2023-06-20 20:55:41 +0900

    [0620] 삼각형과 세 변 - 코드 수정

--- 5000 ~ 10000/BOJ_5073.cpp
+++ 5000 ~ 10000/BOJ_5073.cpp
@@ -6,8 +6,8 @@ using namespace std;
 
 string checkTriangle(vector<int>& v) {
     if (v[0] + v[1] <= v[2]) return "Invalid\n";
-    if (v[0] == v[1] == v[2]) return "Equilateral\n";
-    if (v[0] != v[1] != v[2]) return "Scalene\n";
+    if (v[0] == v[1] && v[1]== v[2]) return "Equilateral\n";
+    if (v[0] != v[1] && v[1] != v[2]) return "Scalene\n";
     return "Isosceles\n";
 }
 
