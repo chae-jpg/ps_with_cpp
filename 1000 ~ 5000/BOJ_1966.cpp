0dc7ac82674b5a08a96ac890d4b8f8335b939c7d
cakenpeace <chwon02@naver.com>
2023-06-20 20:31:03 +0900

    [0620] 프린터 큐 - 풀려고 했...음^^

---
+++ 1000 ~ 5000/BOJ_1966.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    int t, n, m, input;
+    cin >> t;
+
+    while (t--) {
+        cin >> n >> m;
+        queue<int> q;
+        while (n--) {
+            q.push(input);
+        }
+
+        
+
+    }
+
+
+
+
+
+}
