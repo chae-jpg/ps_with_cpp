17d171e6cfab9ecbc8236cc04f60805b9c8f748d
cakenpeace <chwon02@naver.com>
2023-06-22 05:18:57 +0900

    [0622] 프린터 큐

--- 1000 ~ 5000/BOJ_1966.cpp
+++ 1000 ~ 5000/BOJ_1966.cpp
@@ -1,21 +1,45 @@
 #include <iostream>
 #include <queue>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main() {
-    int t, n, m, input;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int t, n, m, input, cnt;
     cin >> t;
 
     while (t--) {
+        cnt = 0;
         cin >> n >> m;
         queue<int> q;
-        while (n--) {
+        vector<int> v;
+        for(int i = 0; i < n; i++) {
+            cin >> input;
             q.push(input);
+            v.push_back(input);
         }
+        sort(v.begin(), v.end());
 
-        
-
+        while (true) {
+            if(q.front() == v[v.size() - 1]) {
+                cnt++;
+                q.pop();
+                v.pop_back();
+                if (m == 0) {
+                    break;
+                }
+            }
+            else {
+                q.push(q.front());
+                q.pop();
+            }
+            m = (m == 0) ? q.size()-1 : m - 1;
+        }       
+        cout << cnt << '\n';
     }
 
 
