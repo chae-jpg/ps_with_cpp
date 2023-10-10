0e8dcf2b35734e358e0472fe5e01c1c6f6d79137
cakenpeace <chwon02@naver.com>
2023-10-10 17:29:00 +0900

    [1010] n과 m (9) - 실버 2 - 백트래킹

---
+++ 15001 ~ 20000/BOJ_15663.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> arr;
+vector<int> answer;
+vector<bool> visited;
+
+void backtrack(int cnt, int m, int n) {
+    if (cnt == m) {
+        for (int a : answer) {
+            cout << a << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+    for (int i = 0; i < n; i++) {
+        if (visited[i]) continue;
+        if (i && arr[i] == arr[i-1] && !visited[i-1]) continue;
+        visited[i] = true;
+        answer[cnt] = arr[i];
+        backtrack(cnt+1, m, n);
+        visited[i] = false;
+    }
+}
+
+int main() {
+    int n, m;
+
+    cin >> n >> m;
+    arr.assign(n, 0);
+    visited.assign(n, false);
+    answer.assign(m, 0);
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    sort(arr.begin(), arr.end());
+
+
+    backtrack(0, m, n);
+
+}
