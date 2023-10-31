da74bd6f229a767ea269e429f37bb4b36b0d4c1a
cakenpeace <chwon02@naver.com>
2023-10-31 22:08:13 +0900

    [1031] 쿼드트리 - 실버 1 - 재귀

--- 1000 ~ 5000/BOJ_1992.cpp
+++ 1000 ~ 5000/BOJ_1992.cpp
@@ -7,25 +7,25 @@ using namespace std;
 
 typedef pair<int, int> pi;
 
-string answer = "(", temp = "";
+string answer = "";
 
-void quadtree(int n, const vector<vector<char>>& tree, pi l, pi r) {
-    if (n == 1) {
-        temp += tree[l.first][l.second];
-        if (temp.length() == 4) {
-            if (temp == "1111") answer += '1';
-            else if (temp == "0000") answer += '0';
-            else answer += "("+ temp + ")";
-            temp.clear();
+void quadtree(int n, const vector<vector<char>>& tree, pi l) {
+    char cmp = tree[l.first][l.second];
+
+    for (int i = l.first; i < l.first + n; i++) {
+        for (int j = l.second; j < l.second + n; j++) {
+            if (tree[i][j] != cmp) {
+                answer += '(';
+                quadtree(n/2, tree, l);
+                quadtree(n/2, tree, {l.first, l.second + n/2});
+                quadtree(n/2, tree, {l.first + n/2, l.second});
+                quadtree(n/2, tree, {l.first + n/2, l.second + n/2});
+                answer += ')';
+                return;
+            }
         }
-        return;
     }
-    quadtree(n/2, tree, l, {r.first/2, r.second/2});
-    quadtree(n/2, tree, {l.first, r.second / 2}, {r.first/2, r.second});
-    quadtree(n/2, tree, {r.first/2+1, l.second}, {r.first, r.second/2});
-    quadtree(n/2, tree, {r.first/2+1, r.second/2+1}, r);
-
-
+    answer += cmp;
 
 }
 
@@ -42,5 +42,6 @@ int main() {
         }
     }
 
-    quadtree(n, tree, {0, 0}, {n-1, n-1});
+    quadtree(n, tree, {0, 0});
+    cout << answer;
 }
