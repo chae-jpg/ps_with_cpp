0c59de0268292b579aed3f9661b25aa0878cb1b6
cakenpeace <chwon02@naver.com>
2023-11-14 19:36:15 +0900

    [1114] 회전초밥 - 실버 1 - 투포인터

---
+++ 1000 ~ 5000/BOJ_2531.cpp
@@ -0,0 +1,58 @@
+//회전초밥 - 실버 1
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, d, k, c, answer = 0;
+    vector<int> sushi;
+    cin >> n >> d >> k >> c;
+    vector<int> ate;
+    sushi.assign(n, 0);
+    ate.assign(d+1, 0);
+
+    for (int i = 0; i < n; i++) {
+        cin >> sushi[i];
+    }
+
+    int l = 0, r = k-1, cnt = 0;
+    for (int i = l; i <= r; i++) {
+        if (!ate[sushi[i]]) {
+            cnt++;
+        }
+        ate[sushi[i]]++;
+    }
+    if (!ate[c]) cnt++;
+    answer = max(answer, cnt);
+    if (!ate[c]) cnt--;
+
+    //r : 끝까지
+    while (r < n-1) {
+        l++; r++;
+        ate[sushi[l-1]]--;
+        if (!ate[sushi[l-1]]) cnt--;
+        if (!ate[sushi[r]]) cnt++;
+        ate[sushi[r]]++;
+        if (!ate[c]) cnt++;
+        answer = max(answer, cnt);
+        if (!ate[c]) cnt--;
+    }
+    r = 0, l++;
+    //l : 끝까지
+    while (l < n) {
+        ate[sushi[l-1]]--;
+        if (!ate[sushi[l-1]]) cnt--;
+        if (!ate[sushi[r]]) cnt++;
+        ate[sushi[r]]++;
+        if (!ate[c]) cnt++;
+        answer = max(answer, cnt);
+        if (!ate[c]) cnt--;
+        l++; r++;
+    }
+    cout << answer;
+}
