12b503db7f6bb4794942a6d3a7565cb75f690ef7
cakenpeace <chwon02@naver.com>
2023-11-28 17:10:09 +0900

    [1128] 문자열 폭발 - 골드 4 - 푸는 중

--- 5000 ~ 10000/BOJ_9935.cpp
+++ 5000 ~ 10000/BOJ_9935.cpp
@@ -7,61 +7,54 @@ using namespace std;
 
 int main() {
     string str, target;
-    stack<char> l, r;
+    stack<char> ans, t;
+    stack<int> check;
     cin >> str >> target;
 
-    for (int i = str.length()-1; i >= 0; i--) {
-        r.push(str[i]);
+    for (int i = 0; i < str.length(); i++) {
+        if (str[i] == target[0]) {
+            if (target.length() == 1) continue;
+            check.push(0);
+            ans.push(str[i]);
+            continue;
+        }
+        ans.push(str[i]);
+
+        if (!check.empty()) {
+            if (target[check.top()+1] == str[i]) check.push(check.top()+1);
+            else {
+                while(check.top()) {
+                    check.pop();
+                }
+                check.pop();
+            }
+        }
+        if (!check.empty() && check.top() == target.length()-1) {
+            for (int j = 0; j < target.size(); j++) ans.pop();
+            while(check.top()) check.pop();
+            check.pop();
+        }
     }
-
-    int idx = 0;
-    string temp ="";
-    bool flag = false;
-
-    while (!r.empty()) {
-        if (flag) {
-            if (target[++idx] == r.top()) {
-                temp += r.top();
-            } else {
-                temp = "";
-                idx = 0;
-                flag = false;
+    if (ans.empty()) {
+        if (check.empty()) cout << "FRULA";
+        else {
+            while (!check.empty()) {
+                t.push(target[check.top()]);
+                check.pop();
+            }
+            while (!t.empty()) {
+                cout << t.top();
+                t.pop();
             }
         }
-        if(r.top() == target[0]) {
-            flag = true;
-            temp = "";
-            temp += r.top();
+    } else {
+        while (!ans.empty()) {
+            t.push(ans.top());
+            ans.pop();
         }
-        if (idx == target.length() - 1) {
-            flag = false;
-            r.pop();
-            for (int i = 0; i < idx; i++) {
-                l.pop();
-            }
-            if (!l.empty() && l.top() == target[0]) {
-                r.push(l.top());
-                l.pop();
-                idx=0;
-                continue;
-            }
-            idx=0;
-        }
-
-            if (!r.empty()) {
-                l.push(r.top());
-                r.pop();
-            }
-    }
-    if (l.empty()) cout << "FRULA";
-    else {
-        while (!l.empty()) {
-            r.push(l.top());
-            l.pop();
-        }
-        while (!r.empty()) {
-            cout << r.top();
-            r.pop();
+        while (!t.empty()) {
+            cout << t.top();
+            t.pop();
         }
     }
 
