a80197f5fef9bb1ac11776c87636bec00a0dec4d
cakenpeace <chwon02@naver.com>
2023-12-06 00:00:47 +0900

    [1206] 집합의 표현 - 골드 5 - 유니언 파인드

---
+++ 1000 ~ 5000/BOJ_1717.cpp
@@ -0,0 +1,46 @@
+//집합의 표현 - 골드 5
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, m, a, b, c;
+vector<int> arr;
+
+int find(int x) {
+    if (x == arr[x]) return x;
+    return arr[x] = find(arr[x]);
+}
+
+void solve() {
+    //union
+    if (!a) {
+        int x = find(b);
+        int y = find(c);
+        arr[y] = x;
+        return;
+    }
+    //find
+    int x = find(b);
+    int y = find(c);
+    if (x==y) cout << "YES\n";
+    else cout << "NO\n";
+}
+
+int main() {
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> n >> m;
+    arr.assign(n+1, 0);
+    for (int i = 0; i <= n; i++) {
+        arr[i] = i;
+    }
+
+    for (int i = 0; i < m; i++) {
+        cin >> a >> b >> c;
+        solve();
+    }
+
+
+}
