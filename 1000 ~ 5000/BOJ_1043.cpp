b8e2aa03981764088736fdf82490bd78e2575638
cakenpeace <chwon02@naver.com>
2023-12-06 23:25:02 +0900

    [1206] 거짓말 - 골드 4 - 유니언 파인드

--- 1000 ~ 5000/BOJ_1043.cpp
+++ 1000 ~ 5000/BOJ_1043.cpp
@@ -1,70 +1,64 @@
 //거짓말 - 골드 4
 
 #include <iostream>
-#include <set>
 #include <vector>
 
 using namespace std;
 
-int m;
-vector<vector<int>> party_ppl, ppl_party;
-vector<bool> can_join;
-set<int> know_truth;
+vector<int> parent, truth;
+vector<vector<int>> party;
 
-void search(int& lie_cnt, int p) {
-    if (know_truth.find(p) != know_truth.end()) return;
-    for (int party : ppl_party[p]) {
-        if (can_join[party]){
-            can_join[party] = false;
-            lie_cnt++;
-            for (int n : party_ppl[party]) {
-                search(lie_cnt, n);
-            }
-        }
+int find(int n) {
+    if (parent[n] == n) return n;
 
-    }
+    return parent[n] = find(parent[n]);
+}
+
+void unionF(int a, int b) {
+    a = find(a);
+    b = find(b);
+
+    if (a != b) parent[b] = a;
 }
 
 int main() {
-    ios_base ::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    int n, t, lie_cnt = 0, pn;
+    int n, m, t, cnt = 0;
 
     cin >> n >> m;
-    party_ppl.assign(m+1, vector<int>());
-    ppl_party.assign(n+1, vector<int>());
-    can_join.assign(m+1, true);
+    parent.assign(n+1, 0);
+    party.assign(m, vector<int> ());
+    for (int i = 1; i <= n; i++) {
+        parent[i] = i;
+    }
 
-    int truth_cnt;
-    cin >> truth_cnt;
-    for (int i = 0; i < truth_cnt; i++) {
+    int a; cin >> a;
+    while (a--) {
         cin >> t;
-        know_truth.insert(t);
+        truth.push_back(t);
     }
 
-    for (int i = 1; i <= m; i++) {
-        cin >> pn;
-        while (pn--) {
+    for (int j = 0; j < m; j++) {
+        cin >> a;
+        for (int i = 0; i < a; i++) {
             cin >> t;
-            party_ppl[i].push_back(t);
-            ppl_party[t].push_back(i);
+            party[j].push_back(t);
+        }
+        if (a > 1) {
+            for (int i = 0; i < a-1; i++) {
+                unionF(party[j][0], party[j][i+1]);
+            }
         }
     }
-
-    for (auto iter = know_truth.begin(); iter != know_truth.end(); iter++) {
-        for (int p : ppl_party[*iter]) {
-            can_join[p] = false;
-            lie_cnt++;
+    for (int i = 0; i < m; i++) {
+        bool flag = true;
+        for (int t : truth) {
+            if (find(t) == find(party[i][0])) {
+                flag = false;
+                break;
+            }
         }
+        if (flag) cnt++;
     }
+    cout << cnt;
 
-    for (int i = 1; i <= m; i++) {
-        if (can_join[i]) continue;
-        for (int p : party_ppl[i]) {
-            search(lie_cnt, p);
-        }
-    }
-
-    cout << (m - lie_cnt >= 0) ? m-lie_cnt : 0;
 }
