e5656bce196ff70d49248a2d4603490abd88d06e
cakenpeace <chwon02@naver.com>
2024-01-26 15:44:45 +0900

    [0126] 수 나누기 게임 - 골드 5 - 정수론

--- 25000~30000/BOJ_27172.cpp
+++ 25000~30000/BOJ_27172.cpp
@@ -4,22 +4,10 @@
 
 using namespace std;
 
-vector<int> v, ans;
-vector<int> is_prime;
-unordered_map<int, int> m;
-
-void findPrime() {
-    is_prime.assign(1e6+1, 0);
-    is_prime[1] = false;
-    for (int i = 2; i * i <= 1e6; i++) {
-        if (is_prime[i]) continue;
-        for (int j = i*i; j <= 1e6; j += i) {
-            if (!is_prime[j]) {
-                is_prime[j] = i;
-            }
-        }
-    }
-}
+vector<int> v;
+vector<vector<int>> divisor;
+set<int> s;
+map<int, int> cnt;
 
 int main() {
     ios_base ::sync_with_stdio(false);
@@ -28,35 +16,32 @@ int main() {
     int n;
     cin >> n;
     v.assign(n, 0);
-    ans.assign(n, 0);
+    divisor.assign(n, vector<int>());
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++){
         cin >> v[i];
-        m[v[i]] = i;
+        s.insert(v[i]);
     }
-    sort(v.begin(), v.end());
-    findPrime();
 
     for (int i = 0; i < n; i++) {
-        if (!is_prime[v[i]]) {
-            for (int j = 0; j <= i-1; j++) {
-                if (v[j] % v[i] == 0) {
-                    ans[m[v[j]]]--;
-                    ans[m[v[i]]]++;
-                }
-            }
-        }
-        for (int j = i+1; j < n; j++) {
-            if (is_prime[v[j]]) continue;
-            if (v[j] % v[i] == 0) {
-                ans[m[v[j]]]--;
-                ans[m[v[i]]]++;
+        for (int j = 1; j * j <= v[i]; j++) {
+            if (v[i] % j == 0) {
+                divisor[i].push_back(j);
+                if (j*j != v[i]) divisor[i].push_back(v[i] / j);
             }
         }
     }
 
-    for (auto i : ans) {
-        cout << i << ' ';
+    for (int i = 0; i < n; i++) {
+        for (int j : divisor[i]) {
+            // 약수 셋에서 발견 시
+            if (s.find(j) != s.end()) {
+                cnt[v[i]]--;
+                cnt[j]++;
+            }
+        }
     }
-
+    for (int i = 0; i < n; i++) {
+        cout << cnt[v[i]] << ' ';
+    }
 }
