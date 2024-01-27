3deb8313fb304887c26f3313919f9993beb9be31
cakenpeace <chwon02@naver.com>
2024-01-28 02:19:47 +0900

    [0126] 트리의 지름 - 골드 4 - 트리, dp

--- 1000 ~ 5000/BOJ_1967.cpp
+++ 1000 ~ 5000/BOJ_1967.cpp
@@ -1,41 +1,65 @@
 //트리의 지름 - 골드 4
 
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 typedef pair<int, int> pi;
 
-int max_height = 0, sum = 0;
+int v;
+vector<vector<pi>> tree;
+vector<int> len;
+vector<bool> visited;
 
-void traverse(vector<vector<pi>>& tree, int node) {
-    if (!tree[node].size()) {
-        max_height = max(sum, max_height);
-        return;
+void calcLen(int n) {
+    int cnt = 0;
+    visited[n] = true;
+    for (auto i : tree[n]) {
+        if (visited[i.first]) continue;
+        if (!len[i.first]) calcLen(i.first);
+        cnt = max(len[i.first] + i.second, cnt);
     }
-    for (int i = 0; i < tree[node].size(); i++) {
-        sum += tree[node][i].second;
-        traverse(tree, tree[node][i].first);
-        sum -= tree[node][i].second;
+    len[n] = cnt;
+}
+
+int calcDia(int n) {
+    visited[n] = true;
+    int ans = 0;
+    int max1 = 0, max2 = 0;
+    for (auto i : tree[n]) {
+        if (visited[i.first]) continue;
+        if (len[i.first] + i.second > max1) {
+            max2 = max1;
+            max1= len[i.first] + i.second;
+        }
+        else if (len[i.first] + i.second > max2) {
+            max2 = len[i.first] + i.second;
+        }
     }
+    int max_child_d = 0;
+    for (auto i : tree[n]) {
+        if (visited[i.first]) continue;
+        max_child_d = max(max_child_d, calcDia(i.first));
+    }
+    return max(max_child_d, max1+max2);
 }
 
 int main() {
-    int n;
-    // 행 : 부모노드, 원소 : {자식 노드, 가중치}
-    vector<vector<pi>> tree;
-    cin >> n;
-    tree.assign(n + 1, vector<pi>(0));
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> v;
+    tree.assign(v+1, vector<pi>());
 
-    for (int i = 0; i < n-1; i++) {
+    for (int i = 0; i < v-1; i++) {
         int a, b, c;
         cin >> a >> b >> c;
-        tree[a].push_back({b, c});
+        tree[a].push_back({b,c});
+        tree[b].push_back({a, c});
     }
-
-    traverse(tree, 1);
-    cout << max_height;
-
+    visited.assign(v+1, false);
+    len.assign(v+1, 0);
+    calcLen(1);
+    visited.assign(v+1, false);
+    cout << calcDia(1);
 
 }
