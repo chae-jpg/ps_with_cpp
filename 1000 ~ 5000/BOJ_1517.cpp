a9327cec4ecb68cc44e8ffc4ac4178d5d03f8b23
cakenpeace <chwon02@naver.com>
2024-01-30 01:40:29 +0900

    [0130] 버블 소트 - 플래티넘 5 - 정렬

--- 1000 ~ 5000/BOJ_1517.cpp
+++ 1000 ~ 5000/BOJ_1517.cpp
@@ -1,36 +1,61 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+//버블 소트 - 플래티넘 5
+
+#include <bits/stdc++.h>
 
 using namespace std;
 
-typedef pair<int, int> pi;
+long long cnt = 0;
+
+vector<long long> arr;
+vector<long long> sorted;
+
+void merge(int l, int mid, int r) {
+    int i = l, j = mid+1, k = l;
+
+    while (i <=mid && j <= r) {
+        if (arr[i]<=arr[j]) {
+            sorted[k++] = arr[i++];
+        }
+        else {
+            cnt += (mid - i)+1;
+            sorted[k++] = arr[j++];
+        }
+    }
+    while (i <= mid) {
+        sorted[k++] = arr[i++];
+    }
+    while (j <= r) {
+        sorted[k++] = arr[j++];
+    }
+    for (i = l; i<= r; i++) {
+        arr[i] = sorted[i];
+    }
+}
+
+void divide(int l, int r) {
+    if (l < r) {
+        int mid = (l+r)/2;
+        divide(l, mid);
+        divide(mid+1, r);
+        merge(l, mid, r);
+    }
+}
 
 int main() {
-    int n, cnt = 0, low = 500000, idx = 0;
-    cin >> n;
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
 
-    vector<pi> arr(n, {0, 0});
+    int n;
+    cin >> n;
+    arr.assign(n, 0);
+    sorted.assign(n, 0);
 
     for (int i = 0; i < n; i++) {
-        cin >> arr[i].first;
-        arr[i].second = i;
+        cin >> arr[i];
     }
 
-    sort(arr.begin(), arr.end());
-
-    for (int i = n-1; i >= 0; i--) {
-        if (arr[i].second == i) continue;
-        if (arr[i].second > low) {
-            cnt += abs(arr[i].second - low - i - idx);
-        } else {
-            cnt += abs(arr[i].second - i);
-        }
-        if (arr[i].second < low) {
-            low = arr[i].second;
-            idx++;
-        }
-    }
+    divide(0, n-1);
     cout << cnt;
+
 
 }
