8f8eec68db8212c1a6e1ddb943dabecdb13a75f3
cakenpeace <chwon02@naver.com>
2024-03-10 22:12:54 +0900

    [0310] 집합 - 실버 5 - 비트마스킹

--- 10001 ~ 15000/BOJ_11723.cpp
+++ 10001 ~ 15000/BOJ_11723.cpp
@@ -5,13 +5,32 @@ using namespace std;
 
 
 int main() {
-    int m;
+    int m, x = 0;
+    int total = 0b0;
     cin >> m;
     while (m--) {
+        string s;
+        cin >> s;
 
-
-
-
+        if (s == "add") {
+            cin >> x;
+            total |= (1 << x);
+        } else if (s == "remove") {
+            cin >> x;
+            total &= ~(1 << x);
+        } else if (s == "check") {
+            cin >> x;
+            if ((total & (1 << x)) == 0) cout << 0 << '\n';
+            else cout << 1 << '\n';
+        } else if (s == "toggle") {
+            cin >> x;
+            if ((total & (1 << x)) == 0) total |= (1<<x);
+            else total &= ~(1 << x);
+        } else if (s == "all") {
+            total = 2097150;
+        } else {
+            total = 0;
+        }
     }
 
 
