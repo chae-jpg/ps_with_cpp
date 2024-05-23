3fb1db83f40c77df09a13765917bcf1eeec477e6
cakenpeace <chwon02@naver.com>
2024-05-23 23:02:18 +0900

    [0523] 아기 상어 - 구현

--- 15001 ~ 20000/BOJ_16236.cpp
+++ 15001 ~ 20000/BOJ_16236.cpp
@@ -16,18 +16,17 @@ pi shark_cur;
 
 bool comp(pi a, pi b) {
     if (a.first == b.first) return a.second < b.second;
-    return a.second < b.second;
+    return a.first < b.first;
 }
 
 int solve() {
     int ans = 0;
     while (fish_cnt) {
         queue<pi> q;
-        vector<vector<bool>> visited(n, vector<bool> (n, false));
         bool found = false;
         q.push(shark_cur);
         int time = 0;
-        visited[shark_cur.first][shark_cur.second] = true;
+        vector<vector<bool>> visited (n, vector<bool> (n, false));
         while (!q.empty()) {
             vector<pi> possible;
             int len = q.size();
@@ -35,6 +34,7 @@ int solve() {
             for (int j = 0; j < len; j++) {
                 int x = q.front().first, y = q.front().second;
                 q.pop();
+                visited[x][y] = true;
                 for (int i = 0; i < 4; i++) {
                     int nx = x + dx[i], ny = y + dy[i];
                     if (nx < 0 || nx >= n || ny < 0 || ny >= n || visited[nx][ny]) continue;
@@ -47,28 +47,26 @@ int solve() {
                     }
                     visited[nx][ny] = true;
                     q.push({nx, ny});
-                    //나보다 작은 물고기 -> 먹고 그 쪽으로 이동, 만약 먹은 개수 차면 크기 키워주기
-                    if (!possible.empty()) {
-                        sort(possible.begin(), possible.end(), comp);
-                        pi next = possible[0];
-                        ans += time;
-                        shark_cur = next;
-                        board[next.first][next.second] = 0;
-                        eat_cnt++;
-                        if (eat_cnt == shark_size) {
-                            eat_cnt = 0;
-                            shark_size++;
-                        }
-                        fish_cnt--;
-                        found = true;
-                    }
-
                 }
-                if (found) break;
+            }
+            //나보다 작은 물고기 -> 먹고 그 쪽으로 이동, 만약 먹은 개수 차면 크기 키워주기
+            if (!possible.empty()) {
+                sort(possible.begin(), possible.end(), comp);
+                pi next = possible[0];
+                ans += time;
+                shark_cur = next;
+                board[next.first][next.second] = 0;
+                eat_cnt++;
+                if (eat_cnt == shark_size) {
+                    eat_cnt = 0;
+                    shark_size++;
+                }
+                fish_cnt--;
+                found = true;
             }
             if (found) break;
         }
-        if (!found) return ans;
+        if (!found && fish_cnt) return ans;
     }
     return ans;
 }
